ab8656c317db2e7d034bd318758a8a2ca91b6bdb
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-05-07 14:21:01 -0300

    solution exercise number 4.

--- src/Semana01/Exercicio.java
+++ src/Semana01/Exercicio.java
@@ -1,4 +1,22 @@
 package Semana01;
 
+import java.util.Scanner;
+
 public class Exercicio {
+    // Faça dentro do escopo do método main uma rotina
+    // através de laço de repetição while um programa
+    // que receberá nomes de alunos até que seja digitado
+    // "fim”
+
+    public static void main(String[] args) {
+
+        Scanner entry = new Scanner(System.in);
+        System.out.println("Enter student name or the word FIM to stop:");
+        String inputValue = entry.next();
+
+        while (!inputValue.equalsIgnoreCase("FIM")) {
+            System.out.println("Enter student name or the word FIM to stop:");
+            inputValue = entry.next();
+        }
+    }
 }
